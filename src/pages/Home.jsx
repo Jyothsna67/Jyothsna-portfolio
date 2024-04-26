8c2734393ae2b4c5237f03a38b699b1332933164
Jyothsna67 <peddintijyothsna06@gmail.com>
2024-04-26 19:31:40 +0530

    Animation commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ import Plane from '../models/Plane';
         </div> */}
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1)
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -57,6 +58,7 @@ const Home = () => {
                 rotation={islandRotation}
                 isRotating={isRotating}
                 setIsRotating={setIsRotating}
+                setCurrentStage={setCurrentStage}
                 />
                 <Plane 
                 isRotating={isRotating}
