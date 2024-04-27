4006071893a8137565bc846c0220606fccb4e837
Jyothsna67 <peddintijyothsna06@gmail.com>
2024-04-27 15:59:39 +0530

    Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,127 @@
-import React from 'react'
+import { useState, useRef, Suspense } from 'react'
+import emailjs from '@emailjs/browser'
+import { Canvas } from "@react-three/fiber";
+// import { Fox }  from "../models/Fox";
+// import { Loader } from '../components/Loader';
 
 const Contact = () => {
+  const formRef = useRef(null);
+  const [form, setForm] = useState({name: '', email: '', message:''})
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Jyothsna",
+        from_email: form.email,
+        to_email: 'peddintijyothsna06@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+    ).then(() => {
+      setIsLoading(false);
+      //TODO: Show success message
+      //TODO: Hide an alert
+
+      setForm({ name: '', email: '', message: ''});
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+      //TODO: show error message
+    })
+  };
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in Touch</h1>
+        <form
+        className='w-full flex flex-col gap-7 mt-14'
+        onSubmit={handleSubmit}
+        >
+          <label className='text-black-500 font-semobold'>
+            Name
+            <input
+            type="text"
+            name="name"
+            className='input'
+            placeholder='john'
+            required
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semobold'>
+            Email
+            <input
+            type="email"
+            name="email"
+            className='input'
+            placeholder='john@gmail.com'
+            required
+            value={form.email}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semobold'>
+            Your message
+            <textarea
+            name="message"
+            rows={4}
+            className='textarea'
+            placeholder='Let me know how I can help you!'
+            required
+            value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+          <button
+          type='submit'
+          className='btn'
+          disabled={isLoading}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          >
+            {isLoading ? 'sending...' : 'send message'}
+          </button>
+
+        </form>
+      </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        {/* <Canvas
+          camera={{
+            position: [0, 0, 5]
+          }}
+        >
+          <Suspense fallback={Loader}>
+            <Fox 
+            position={[0.5, 0.35, 0]}/>
+          </Suspense>
+
+        </Canvas> */}
+
+      </div>
+
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,8 @@ import  Island  from '../models/Island';
 import Sky from '../models/Sky';
 import Bird from '../models/Bird';
 import Plane from '../models/Plane';
+import HomeInfo from '../components/HomeInfo';
 
- {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-            POPUP
-        </div> */}
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1)
@@ -42,6 +40,9 @@ const Home = () => {
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
   return (
     <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+            {currentStage && <HomeInfo currentStage = {currentStage} />}
+        </div>
         <Canvas 
         className={'w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}'}
         camera={{ near: 0.1, far: 1000}}
@@ -51,7 +52,7 @@ const Home = () => {
                 <ambientLight intensity={0.5} />
                 <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
                 <Bird />
-                <Sky />
+                <Sky isRotating={isRotating}/>
                 <Island 
                 position={islandPosition}
                 scale={islandScale}
